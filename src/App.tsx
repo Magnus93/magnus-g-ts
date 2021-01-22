8ef06fc9d0aa6c8f99be2ce38bc4032bf87ffc64
Magnus93 <magnus.ja.gustafsson@gmail.com>
2021-01-22 20:48:02 +0100

    Add nav bar with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
+      <div className="header">
+        <input type="checkbox" id="nav-toggle" className="nav-toggle" />
         <label htmlFor="nav-toggle" className="nav-toggle-label">
           <span></span>
         </label>
-      </header>
-      <nav className="nav">
-            <ul className="nav-list">
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/cv">CV</a></li>
-              <li><a href="/about">About Me</a></li>
-            </ul>
-          </nav>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li><a href="/projects">Projects</a></li>
+            <li><a href="/cv">CV</a></li>
+            <li><a href="/about">About Me</a></li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
