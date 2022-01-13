f850b164b666220a64fc8b285fe16048bddcdded
Magnus93 <magnus.ja.gustafsson@gmail.com>
2022-01-13 21:30:50 +0100

    Change theme button

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,58 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProjectsPage from './components/ProjectsPage';
 import AboutPage from "./components/AboutPage";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <input type="checkbox" id="nav-toggle" className="nav-toggle" />
-        <label htmlFor="nav-toggle" className="nav-toggle-label">
-          <span></span>
-        </label>
-        <nav className="nav">
-          <ul className="nav-list">
-            <li><a href="/about">About Me</a></li>
-            <li><a href="/projects">Projects</a></li>
-          </ul>
-        </nav>
+class App extends React.Component<{}, {theme: string}> {
+  constructor(props : any) {
+    super(props);
+    let theme : any = localStorage.getItem('theme');
+    this.state = {
+      theme: theme ? theme : 'light'
+    };
+  }
+
+  toogleTheme() {
+    let theme : string = 'light';
+    if (this.state.theme === 'light') {
+      theme = 'dark';
+    }
+    this.setState({'theme': theme});
+  }
+
+  render() {
+    return (
+      <div className={`App ${this.state.theme}`}>
+        <div className="header">
+          <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+          <label htmlFor="nav-toggle" className="nav-toggle-label">
+            <span></span>
+          </label>
+          <nav className="nav">
+            <ul className="nav-list">
+              <li><a href="/about">About Me</a></li>
+              <li><a href="/projects">Projects</a></li>
+            </ul>
+          </nav>
+          <button onClick={this.toogleTheme.bind(this)}>Change Theme</button>
+        </div>
+        <div>
+          <BrowserRouter>
+              <Switch>
+                <Route path="/projects">
+                  <ProjectsPage/>
+                </Route>
+                <Route path="/about">
+                  <AboutPage/>
+                </Route>
+                <Route path="/">
+                  <ProjectsPage/>
+                </Route>
+              </Switch>
+          </BrowserRouter>
+        </div>
       </div>
-      <div>
-        <BrowserRouter>
-            <Switch>
-              <Route path="/projects">
-                <ProjectsPage/>
-              </Route>
-              <Route path="/about">
-                <AboutPage/>
-              </Route>
-              <Route path="/">
-                <ProjectsPage/>
-              </Route>
-            </Switch>
-        </BrowserRouter>
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
 
 export default App;
