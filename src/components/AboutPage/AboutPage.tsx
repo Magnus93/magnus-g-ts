0aeb0c3f5ccfbdc1b442bb5f72ec7c40addb19dc
Magnus93 <magnus.ja.gustafsson@gmail.com>
2024-08-16 08:18:13 +0200

    Remove mail

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./AboutPage.scss";
 import githubIcon from "./svg/github-icon.svg";
 import linkedinIcon from "./svg/linkedin-icon.svg";
-import mailIcon from "./svg/mail-icon.svg";
 
 interface Link {
 	icon: string
@@ -20,11 +19,6 @@ let links: Link[] = [
 		icon: githubIcon,
 		name: "GitHub.com/Magnus93",
 		href: "https://github.com/Magnus93"
-	},
-	{
-		icon: mailIcon,
-		name: "magnus.ja.gustafsson@gmail.com",
-		href: "mailto:magnus.ja.gustafsson@gmail.com"
 	}
 ];
 
@@ -37,7 +31,7 @@ class AboutPage extends React.Component {
 				<div className="content">
 					<h1>Magnus Gustafsson - <span> Web Developer </span></h1>
 					<p>
-						I work as a fullstack web developer at Intergiro Intl AB, in Uppsala. I especially enjoy frontend work. I worked on StochSD, an Open-Source Simulation Software, written in native JavaScript, HTML, and CSS without any framework, which gave me a good understanding of how JavaScript, the DOM and CSS work. In addition, I've also tried React and Svelte in side-projects. I'm currently most familiar with StencilJS since that's what I use at my job.
+						I work as a web developer at Pax2pay, in Uppsala. I worked on StochSD, an Open-Source Simulation Software, written in native JavaScript, HTML, and CSS without any framework, which gave me a good understanding of how JavaScript, the DOM and CSS work. In addition, I've also tried React and Svelte in side-projects. I'm currently most familiar with StencilJS since that's what I use at my job.
 					</p>
 					{links.map((l) => {
 						return (
